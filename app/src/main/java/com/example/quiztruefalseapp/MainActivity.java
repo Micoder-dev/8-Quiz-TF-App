12b2ff2eae111ae583312bbda6ac21bdf9669a7f
Micoder-dev <micoder.com@gmail.com>
2022-07-22 21:37:01 +0530

    Setup the project by displaying the required Json Data in the LOG

--- app/src/main/java/com/example/quiztruefalseapp/MainActivity.java
+++ app/src/main/java/com/example/quiztruefalseapp/MainActivity.java
@@ -3,12 +3,44 @@ package com.example.quiztruefalseapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RequestQueue mRequestQueue;
+    private String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRequestQueue = VolleySingleton.getInstance().getRequestQueue();
+        url = "https://opentdb.com/api.php?amount=10&category=11&type=boolean";
+
+        JsonObjectRequest filmsJsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+                Log.i("FILMS", response.toString());
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        mRequestQueue.add(filmsJsonObjectRequest);
+
     }
 }
